b19c65e7026da45e3254e535e7594b3eb2704c78
unknown <aditnegarabisnis@gmail.com>
2022-06-07 19:58:15 +0700

    finish submenu mobile navbar

--- src/Components/ModalContent/ModalContent.js
+++ src/Components/ModalContent/ModalContent.js
@@ -1,20 +1,69 @@
 import styles from './ModalContent.module.scss';
 import arrowDownIcon from '../../Assets/Icons/icon-arrow-down.svg';
+import todoListIcon from '../../Assets/Icons/icon-todo.svg';
+import calendarIcon from '../../Assets/Icons/icon-calendar.svg';
+import reminderIcon from '../../Assets/Icons/icon-reminders.svg';
+import planningIcon from '../../Assets/Icons/icon-planning.svg';
 
 const ModalContent = () =>{
     return(
+    <>
       <ul className={styles['modal-content']}>
           <li>
                 <button>
                     <p>Features</p>
                     <img src={arrowDownIcon} alt='arrow down'/>
                 </button>
+                <ul className={styles['modal-content__sub__menu']}>
+                    <li>
+                        <a href='#'>
+                            <img src={todoListIcon} alt='todo list' />
+                            Todo List
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <img src={calendarIcon} alt='calendar' />
+                            Calendar
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <img src={reminderIcon} alt='reminders' />
+                            Reminders
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <img src={planningIcon} alt='planning' />
+                            Planning
+                        </a>
+                    </li>
+                </ul>
           </li>
           <li>
                 <button>
                     <p>Company</p>
                     <img src={arrowDownIcon} alt='arrow down'/>
                 </button>
+                {/* <ul className={styles['modal-content__sub__menu']}>
+                    <li>
+                        <a href='#'>
+                            History
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            Our Team
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            Blog
+                        </a>
+                    </li>
+               
+                </ul> */}
           </li>
           <li className={styles['mt-3']}>
                 <a href="#">Careers</a>
@@ -23,6 +72,12 @@ const ModalContent = () =>{
                 <a href="#">About</a>
           </li>
       </ul>
+      <div className={styles['button-group']}>
+        
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </>
     )
 }
 
